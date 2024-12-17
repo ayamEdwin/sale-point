bcd366d445b8137b01698c8cc593669ff12ba4f4
Edwin Setsoafia <edwinsetsoafia9@gmail.com>
2024-12-17 17:16:33 +0000

    store-manager model setup

---
+++ main/models/store_manager.py
@@ -0,0 +1,28 @@
+"STORE MANAGER"
+class StoreManager:
+    # constructor for store manager
+    def __init__(self,
+        manager_id: str,
+        name: str,
+        username: str
+    ):
+        self.manager_id = manager_id
+        self.name = name
+        self.username = username
+
+    # add product
+    def add_product(self) -> None:
+        pass
+
+    # remove product
+    def remove_product(self) -> None:
+        pass
+
+    # update inventory
+    def update_stock(self) -> None:
+        pass
+
+    # view stock/inventory
+    def view_stock(self) -> None:
+        pass
+    
